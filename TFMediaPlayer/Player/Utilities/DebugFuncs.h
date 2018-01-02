c012abbd1a6704cb5a38b710d7af733d7414200f
shiwei <15811842975@163.com>
2018-01-02 18:08:40 +0800

    1

--- TFMediaPlayer/Player/Utilities/DebugFuncs.h
+++ TFMediaPlayer/Player/Utilities/DebugFuncs.h
@@ -11,18 +11,24 @@
 
 #define TFCheckRetval(statement)\
 if (retval < 0) {\
+char errStr[64];\
+av_strerror(retval, errStr, 64);\
 printf("%s error(%d)\n",statement,retval);\
 }\
 
 #define TFCheckRetvalAndReturn(statement)\
 if (retval < 0) {\
+char errStr[64];\
+av_strerror(retval, errStr, 64);\
 printf("%s error(%d)\n",statement,retval);\
 return;\
 }\
 
 #define TFCheckRetvalAndGotoFail(statement)\
 if (retval < 0) {\
-printf("%s error(%d)\n",statement,retval);\
+char errStr[64];\
+av_strerror(retval, errStr, 64);\
+printf("%s error(%s)\n",statement,errStr);\
 goto fail;\
 }\
 
--- TFMediaPlayer/docs/TFMediaPlayer开发日志.md
+++ TFMediaPlayer/docs/TFMediaPlayer开发日志.md
@@ -1,1 +1,6 @@
 1. 找不到FFmpeg的函数符号，因为在c++文件里导入，使用c++的方式链接，而FFmpeg是c函数符号。使用`extern "C"`
+
+2. 编译优化：
+ * [一个参考](https://gist.github.com/lulalala/7028049)
+ * `Invalid data found when processing input`缺少了demuxer
+ * `av_strerror `解析错误
